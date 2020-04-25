a07c42ef83eea8a3e10fe38a59d7438984508d70
szbitzel <alexionutcraciun@gmail.com>
2020-04-25 11:38:19 -0700

    Yes

--- SQL-PARSE.h
+++ SQL-PARSE.h
@@ -1,1 +1,15 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <string.h>
 
+using namespace std;
+
+
+void _CREATE (string intrare)
+{
+    char fname[50];
+    // Construct file name here.
+    sprintf(fname, "test/%s", intrare);
+    ofstream ostrm(fname);
+}
--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include "SQL-PARSE.h"
 #include <fstream>
+#include <string>
+#include <string.h>
 
-ifstream f ("Tabel.txt");
-ofstream g ("Tabel.txt");
+
+//ifstream f ("Tabel.txt");
+//ofstream g ("Tabel.txt");
 
 using namespace std;
 
@@ -16,24 +19,43 @@ using namespace std;
                d) Datele necesare
                3. Trebuie sa creem prioritati la comenzi (delete sa stearga ori doar un anumit tip de date or toata linia)
                4. Formatare tipuri de date ( sa ii facem diferentiere intre numere, nume , date anuale,etc);
-
+               EXTRA:
+               Numele o sa inceapa cu ' si se termina cu ' exemplu : 'CUCU-CIUHAN Cipran' (o sa ne ajute pentru ca o sa cautam in string-ul dat ' pana la cealalta '
+               Trebuie sa facem si cazul in care lipseste un tip de date, ex: lipseste anul nasterii , programul nostru sare peste coloana aceea
 */
 
 
 int main()
-{
+ {
+
+
+
   while (1)
 
    {
-       switch (commands)
+
+
+       string commands; //aici bagi create
+
+       cin>>commands;
+
+       int ok=1;//
+
+
+       switch (ok)
         {
-         case "CREATE":
+         case "create":
+          //TODO: CREATE va crea ori un tabel ori o baza de date.(creaza un fisier). Acesta va necesita un nume
+          string gol;
+          cin>>gol;
+          cin>>gol;
+          _CREATE(gol);
 
 
 
         }
-   }
 
+   }
 
 
     return 0;
